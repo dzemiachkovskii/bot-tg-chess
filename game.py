dc94e6772568447513c25c3ee9e954667721fe42
dzemiachkovskii <fennyboy2012@gmail.com>
2023-01-28 00:16:03 +0300

    add some code, but got lost and now I don't understand anything

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 class Game:
     def __init__(self):
         self.board = [
-            [2, 3, 4, 5, 6, 4, 3, 2],
-            [1, 1, 1, 1, 1, 1, 1, 1],
+            [8, 9, 10, 11, 12, 10, 9, 8],
+            [7, 7, 7, 7, 7, 7, 7, 7],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,7 +10,81 @@ class Game:
             [1, 1, 1, 1, 1, 1, 1, 1],
             [2, 3, 4, 5, 6, 4, 3, 2]
         ]
+        self.user_turn = True
 
     def move_from(self, x, y):
-        if self.board[x][y] != 0:
-            figure = self.board[x][y]
+        if not self.user_turn:
+            return
+        figure = self.board[x][y]
+        # set figure to ready state
+        if figure in range(1, 7):
+            self.board[x][y] = [self.board[x][y]]
+        # clean all red dots & figure ready state
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if type(self.board[i][j]) == list:
+                    if self.board[i][j][0] == 13:
+                        self.board[i][j] = self.board[i][j][1]
+                    else:
+                        self.board[i][j] = self.board[i][j][0]
+        # /clean all red dots
+        # plot possible steps on board & save current condition of tiles
+        if figure == 1:
+            if x - 2 < 0:
+                return -1
+            self.board[x - 2][y] = [13, self.board[x - 2][y]]
+            if x == 6:
+                self.board[x - 1][y] = [13, self.board[x - 1][y]]
+        elif figure == 2:
+            # up
+            for i in range(x, -1, -1):
+                if i == x:
+                    continue
+                if self.board[i][y] in range(1, 7):
+                    break
+                if self.board[i][y] in range(7, 13):
+                    self.board[i][y] = [13, self.board[i][y]]
+                    break
+                self.board[i][y] = [13, self.board[i][y]]
+            # right
+            for j in range(y, 8):
+                if j == y:
+                    continue
+                if self.board[x][j] in range(1, 7):
+                    break
+                if self.board[x][j] in range(7, 13):
+                    self.board[x][j] = [13, self.board[x][j]]
+                    break
+                self.board[x][j] = [13, self.board[x][j]]
+            # down
+            for i in range(x, 8):
+                if i == x:
+                    continue
+                if self.board[i][y] in range(1, 7):
+                    break
+                if self.board[i][y] in range(7, 13):
+                    self.board[i][y] = [13, self.board[i][y]]
+                    break
+                self.board[i][y] = [13, self.board[i][y]]
+            # left
+            for j in range(y, -1, -1):
+                if j == y:
+                    continue
+                if self.board[x][j] in range(1, 7):
+                    break
+                if self.board[x][j] in range(7, 13):
+                    self.board[x][j] = [13, self.board[x][j]]
+                    break
+                self.board[x][j] = [13, self.board[x][j]]
+        elif figure == 3:
+            pass
+        elif figure == 4:
+            pass
+        elif figure == 5:
+            pass
+        elif figure == 6:
+            pass
+
+    def move_to(self, x, y):
+        if type(self.board[x][y]) == list:
+            self.user_turn = False
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ game = Game()
 
 @bot.message_handler(commands=['start'])
 def greet(m):
-    bot.send_message(m.chat.id, "stop nahui")
+    bot.send_message(m.chat.id, pieces[7])
 
 
 @bot.message_handler(commands=['game'])
@@ -20,15 +20,17 @@ def greet(m):
     bot.send_message(m.chat.id, msg_text, reply_markup=markup)
 
 
-def get_board_markup(coordinates):
+def get_board_markup(board):
     black_tile = False
     btns = []
     for row in range(8):
         for col in range(8):
-            if coordinates[row][col] == 0:
+            if board[row][col] == 0:
                 unit = pieces[0][1] if black_tile else pieces[0][0]
+            elif type(board[row][col]) == list:
+                unit = pieces[board[row][col][0]]
             else:
-                unit = pieces[coordinates[row][col]]
+                unit = pieces[board[row][col]]
             btns.append(types.InlineKeyboardButton(text=unit, callback_data=f'{row}-{col}'))
             black_tile = not black_tile if col < 7 else black_tile
     markup = Keyboa(items=btns, items_in_row=8).keyboard
@@ -37,8 +39,12 @@ def get_board_markup(coordinates):
 
 @bot.callback_query_handler(lambda call: True)
 def btn_handler(c):
-    x, y = c.data.split('-')
-    game.move_from(int(x), int(y))
+    coordinates = c.data.split('-')
+    x, y = int(coordinates[0]), int(coordinates[1])
+    if game.move_from(x, y) == -1:
+        return
+    markup = get_board_markup(game.board)
+    bot.edit_message_reply_markup(c.message.chat.id, c.message.message_id, c.inline_message_id, reply_markup=markup)
 
 
-bot.polling(none_stop=True)
+bot.infinity_polling()
